d8020e5b661042fd0300019ce7a0a681715c3c39
Dolgov <sodorug@gmail.com>
2018-07-21 20:07:23 +0300

    pervie izmenenia

---
+++ Calc/src/base.java
@@ -0,0 +1,13 @@
+public class base {
+    public static void main(String[] args) {
+        int first;
+        first = 10;
+        int second;
+        second = 15;
+        int sum;
+        System.out.println("У нас есть 2 числа "+ first +" и "+second);
+        sum = first + second;
+        System.out.println("Их сумма равна "+sum);
+
+    }
+}
