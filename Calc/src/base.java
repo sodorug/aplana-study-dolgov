a40107b81ed659244ea31027b9f126c403060024
Dolgov <sodorug@gmail.com>
2018-07-23 10:00:27 +0300

    добавлен вывод в формате 10,1111

--- Calc/src/base.java
+++ Calc/src/base.java
@@ -10,24 +10,28 @@ public class base {
         double second = scan.nextDouble();
         System.out.println("Вы ввели число "+second);
         System.out.println("Выберите тип выполняемой операции + - * /");
-        double sum = 0;
+        double sum;
         String operation = scan.next();
         if (operation.startsWith("+")){
             sum = first+second;
-            System.out.println("Сумма значений "+sum);
+            System.out.println("Сумма значений ");
+            System.out.printf("%.4f",+sum);
         }
         else if(operation.startsWith("-")){
             sum = first - second;
-            System.out.println("Разность первого и второго числа "+sum);
+            System.out.println("Произведение чисел равна ");
+            System.out.printf("%.4f",+sum);
         }
         else if(operation.startsWith("*")) {
             sum = first * second;
-            System.out.println("Произведение чисел равна " + sum);
+            System.out.println("Произведение чисел равна ");
+            System.out.printf("%.4f",+sum);
         }
-        else if(operation.startsWith("-")) {
+        else if(operation.startsWith("/")) {
             sum = first / second;
-            System.out.println("Отношение чисел равна " + sum);
+            System.out.println("Отношение чисел равна ");
+            System.out.printf("%.4f",+sum);
         }
-    System.out.println(sum);
+
     }
 }
