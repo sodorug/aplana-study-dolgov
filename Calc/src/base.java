0e2b45a280d28ddc6a1e99c44e23f10cf2a2934e
Dolgov <sodorug@gmail.com>
2018-07-23 09:33:29 +0300

    Написан полный код калькулятора

--- Calc/src/base.java
+++ Calc/src/base.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class base {
     public static void main(String[] args) {
-        int first;
-        first = 10;
-        int second;
-        second = 15;
-        int sum;
-        System.out.println("У нас есть 2 числа "+ first +" и "+second);
-        sum = first + second;
-        System.out.println("Их сумма равна "+sum);
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        double first = scan.nextDouble();
+        System.out.println("Вы ввели число "+first);
+        System.out.println("Введите второе число число");
+        double second = scan.nextDouble();
+        System.out.println("Вы ввели число "+second);
+        System.out.println("Выберите тип выполняемой операции + - * /");
+        double sum = 0;
+        String operation = scan.next();
+        if (operation.startsWith("+")){
+            sum = first+second;
+            System.out.println("Сумма значений "+sum);
+        }
+        else if(operation.startsWith("-")){
+            sum = first - second;
+            System.out.println("Разность первого и второго числа "+sum);
+        }
+        else if(operation.startsWith("*")) {
+            sum = first * second;
+            System.out.println("Произведение чисел равна " + sum);
+        }
+        else if(operation.startsWith("-")) {
+            sum = first / second;
+            System.out.println("Отношение чисел равна " + sum);
+        }
+    System.out.println(sum);
     }
 }
